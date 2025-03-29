c8ff550e52e355b28eda06f28e6f5d2afdabe83c
Anurag Thakur <anurag.zeroit@gmail.com>
2025-03-29 17:24:39 +0530

    Add new files .env file , nodemodule and images folder and some changes in code.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# New-Admin
-New admin panel created by anurag
+# Developer 
+Anurag Thakur
+
+# Location
+Mohali Phase-8B at ( Vanced Solutions);
+
+
+# Changes in Code :-
+1. create .env file and add the database nd port configration.
+2. Create image folder in views folder to store images.
+3. Delete github folder in my project.
--- data/database.js
+++ data/database.js
@@ -1,36 +1,39 @@
 import mongoose from "mongoose";
-import Admin from '../model/admins.js';
+import dotenv from "dotenv";
+// import Admin from '../model/admins.js';
+dotenv.config();
 
-const uri = 'mongodb://localhost:27017/new-admin';
 
 async function connectToDatabase() {
   try {
-    await mongoose.connect(uri, {
+    await mongoose.connect(process.env.MONGODB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
     console.log('Mongodb is connected successfully');
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error.message);
+    // console.error('Error connecting to MongoDB:', error.message);
+    console.log("Error in connected mongodb!");
   }
 }
 
-async function addAdmin(name, email, password, picture, bio) {
-  try {
-    const newAdmin = new Admin({
-      name: name,
-      email: email,
-      password: password,
-      picture: picture,
-      bio: bio
-    });
+// async function addAdmin(name, email, password, picture, bio) {
+//   try {
+//     const newAdmin = new Admin({
+//       name: name,
+//       email: email,
+//       password: password,
+//       picture: picture,
+//       bio: bio
+//     });
 
-    await newAdmin.save();
-    console.log('Admin saved successfully');
-  } catch (err) {
-    console.error('Error saving admin:', err);
-    throw err;
-  }
-}
-export default addAdmin;
-connectToDatabase();
+//     await newAdmin.save();
+//     console.log('Admin saved successfully');
+//   } catch (err) {
+//     console.error('Error saving admin:', err);
+//     throw err;
+//   }
+// }
+
+// export default addAdmin;
+ export default connectToDatabase();
